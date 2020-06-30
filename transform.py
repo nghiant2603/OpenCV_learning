628d6cbcfec0e8f4bf65f9674c29474e8fb3ba92
Nghia Nguyen <nghiant2603@gmail.com>
2020-06-30 07:29:20 +0700

    add transform function

---
+++ transform.py
@@ -0,0 +1,70 @@
+import numpy as np
+import argparse
+import cv2
+
+#create input option
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', help='path to image')
+ap.add_argument("-c", "--coords", help = "comma seperated list of source points")
+args = vars(ap.parse_args())
+
+def order_points(pts): 
+    rect = np.zeros((4,2), dtype='float32')
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+    	[0, 0],
+    	[maxWidth - 1, 0],
+    	[maxWidth - 1, maxHeight - 1],
+    	[0, maxHeight - 1]], dtype = "float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    return warped
+
+def transform(image, coords): 
+    i_img = cv2.imread(image)
+    pts = np.array(eval(coords), dtype = "float32")
+    # apply the four point tranform to obtain a "birds eye view" of
+    # the image
+    o_img = four_point_transform(i_img, pts)
+
+    #cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+    cv2.imshow('window', i_img)
+    cv2.imshow('new', o_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    transform(args['image'], args['coords'])
