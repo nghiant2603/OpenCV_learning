32e1e2bd9efc43e5606a2f6002c67eb8f5e47e1d
Nghia Nguyen <nghiant2603@gmail.com>
2020-07-09 08:50:40 +0700

    .

---
+++ preprocess_image.py
@@ -0,0 +1,48 @@
+### Function : improve the quality of input image
+### Option : 
+###     -i/--image : input image
+import numpy as np
+import argparse
+import cv2
+import imutils
+import matplotlib.pyplot as plt
+from display import *
+
+#create input option
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', help='path to image')
+args = vars(ap.parse_args())
+def preprocess(frame):
+
+    # Increase constrast
+    alpha = 1.2         # constrast : 1.0 -> 3.0
+    #beta = 10            # brightness : 0 -> 100
+    frame = cv2.convertScaleAbs(frame, alpha = alpha)
+
+    # reduce highlight 
+    #hsvImg = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    #hsvImg[...,2] = hsvImg[...,2]*0.6
+    #frame = cv2.cvtColor(hsvImg,cv2.COLOR_HSV2BGR)
+
+    lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB).astype('float32')
+    (l,a,b) = cv2.split(lab)
+    l = l - l.mean() + 128
+    l = np.clip(l, 0, 255)
+    frame = cv2.cvtColor(cv2.merge([l, a, b]).astype('uint8'), cv2.COLOR_LAB2BGR)
+
+    print(l.mean())
+    print(a.mean())
+    print(b.mean())
+    print(l.std())
+    print(a.std())
+    print(b.std())
+
+    return frame
+
+def run (image):
+    frame = cv2.imread(image)
+    frame = preprocess(frame)
+    display(frame)
+
+if __name__ == "__main__":
+    run(args['image'])
