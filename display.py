d543806eb5b43e6432d337b6300fa30653291867
Nghia Nguyen <nghiant2603@gmail.com>
2020-07-09 08:50:20 +0700

    .

--- display.py
+++ display.py
@@ -1,8 +1,13 @@
 import cv2
 
 def display (frame, name='Window') :
-    cv2.namedWindow(name, cv2.WND_PROP_FULLSCREEN)
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow(name, frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def wait () :
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
