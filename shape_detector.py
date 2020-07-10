a3c706bc57d752c688e3af6ea4b3083a3d434d61
Nghia Nguyen <nghiant2603@gmail.com>
2020-07-10 20:02:35 +0700

    .

--- shape_detector.py
+++ shape_detector.py
@@ -5,49 +5,52 @@ import cv2
 import numpy as np
 import argparse
 import imutils
+from preprocess_image import *
+from display import *
 
-#create input option
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', help='path to image')
-args = vars(ap.parse_args())
+def shape_detector (frame, dark_mode=True, n_points=(0, 1000), areas=(0, 100)): 
+    o_frame = frame.copy()
 
-def shape_detector (image): 
-    i_img = cv2.imread(image)
+    frame = preprocess_image(frame) 
 
-    hsvImg = cv2.cvtColor(i_img,cv2.COLOR_BGR2HSV)
-    # decreasing the V channel by a factor from the original
-    hsvImg[...,2] = hsvImg[...,2]*0.6
-    i_img = cv2.cvtColor(hsvImg,cv2.COLOR_HSV2RGB)
-
-    alpha = 2         # constrast : 1.0 -> 3.0
-    beta = 0            # brightness : 0 -> 100
-    aj_img = cv2.convertScaleAbs(i_img, alpha = alpha, beta = beta)
-    
-    gray_img = cv2.cvtColor(i_img, cv2.COLOR_BGR2GRAY)
-    #Blur image to reduce mistake
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-    thresh_img = cv2.threshold(blur_img, 100, 255, cv2.THRESH_BINARY)[1]
+    if dark_mode : 
+        thresh_img = cv2.threshold(blur_img, 150, 255, cv2.THRESH_BINARY)[1]
+    else : 
+        thresh_img = ~cv2.threshold(blur_img, 100, 255, cv2.THRESH_BINARY)[1]
 
-    o_img = cv2.cvtColor(thresh_img, cv2.COLOR_GRAY2BGR)
     cnts = cv2.findContours(thresh_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
+    size = frame.shape
+    full_area = size[0]*size[1]
     for c in cnts:
-        M = cv2.moments(c)
         peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-        if (M["m00"] != 0) : 
-            cX = int(M["m10"]/M["m00"])
-            cY = int(M["m01"]/M["m00"])
-            cv2.drawContours(i_img, [c], -1, (0,255,0), 2)
-            cv2.circle(i_img, (cX, cY), 7, (255, 255, 255), -1)
-            cv2.putText(i_img, str(len(approx)), (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        points = cv2.approxPolyDP(c, 0.04 * peri, True)
+        n_point = len(points)
+        area = 100*cv2.contourArea(c)/(full_area) 
 
-    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-    cv2.imshow('window', np.hstack([i_img, o_img]))
-    #cv2.imshow('window', i_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        if ((n_point >= n_points[0]) and (n_point < n_points[1])) : 
+            if ((area >= areas[0]) and (area < areas[1])) : 
+                M = cv2.moments(c)
+                if (M["m00"] != 0) : 
+                    cX = int(M["m10"]/M["m00"])
+                    cY = int(M["m01"]/M["m00"])
+                    cv2.drawContours(o_frame, [c], -1, (0,255,0), 2)
+                    cv2.circle(o_frame, (cX, cY), 3, (255, 255, 255), -1)
+                    cv2.putText(o_frame, str(int(area)), (cX - 5, cY - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    return o_frame
+
+def run (image):
+    #create input option
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-i', '--image', help='path to image')
+    args = vars(ap.parse_args())
+
+    frame = cv2.imread(image)
+    frame = shape_detector(frame, dark_mode=True, n_points=(0,100), areas=(0, 5)) 
+    display(frame)
 
 if __name__ == "__main__":
-    shape_detector(args['image'])
+    run(args['image'])
