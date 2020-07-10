2a8f6ed667b7c0090d901d427b1b5782e4564008
Nghia Nguyen <nghiant2603@gmail.com>
2020-07-10 07:54:28 +0700

    fish sort_contour

--- preprocess_image.py
+++ preprocess_image.py
@@ -12,17 +12,20 @@ from display import *
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', help='path to image')
 args = vars(ap.parse_args())
-def preprocess(frame):
+def preprocess_image(frame):
 
     # Increase constrast
-    alpha = 1.2         # constrast : 1.0 -> 3.0
+    #alpha = 1.3         # constrast : 1.0 -> 3.0
     #beta = 10            # brightness : 0 -> 100
-    frame = cv2.convertScaleAbs(frame, alpha = alpha)
+    #frame = cv2.convertScaleAbs(frame, alpha = alpha)
 
     # reduce highlight 
     #hsvImg = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    #hsvImg[...,2] = hsvImg[...,2]*0.6
+    #hsvImg[...,2] = hsvImg[...,2]*0.8
     #frame = cv2.cvtColor(hsvImg,cv2.COLOR_HSV2BGR)
+
+    #kernel = np.ones((5,5),np.uint8)
+    #erode_img = cv2.erode(thresh_img, kernel, 1)
 
     lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB).astype('float32')
     (l,a,b) = cv2.split(lab)
@@ -30,18 +33,18 @@ def preprocess(frame):
     l = np.clip(l, 0, 255)
     frame = cv2.cvtColor(cv2.merge([l, a, b]).astype('uint8'), cv2.COLOR_LAB2BGR)
 
-    print(l.mean())
-    print(a.mean())
-    print(b.mean())
-    print(l.std())
-    print(a.std())
-    print(b.std())
+    #print(l.mean())
+    #print(a.mean())
+    #print(b.mean())
+    #print(l.std())
+    #print(a.std())
+    #print(b.std())
 
     return frame
 
 def run (image):
     frame = cv2.imread(image)
-    frame = preprocess(frame)
+    frame = preprocess_image(frame)
     display(frame)
 
 if __name__ == "__main__":
--- sort_contour.py
+++ sort_contour.py
@@ -6,13 +6,14 @@ import numpy as np
 import argparse
 import imutils
 from display import *
+from preprocess_image import *
 
 #create input option
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', help='path to image')
 args = vars(ap.parse_args())
 
-def sort_contour (frame, method = 'XA'):    # XA : sort X_axis Ascending , XD : sort x_axis Descending, YA, YD    if (method == 'LT') : 
+def sort_contour (frame, method = 'XA', dark_mode=True):    # XA : sort X_axis Ascending , XD : sort x_axis Descending, YA, YD    if (method == 'LT') : 
     if (method == 'XA') :
         y_axis = 0          # 0 : sort x_axis - 1 : sort y_axis
         reverse = False     # False : ascending - True : descending
@@ -28,22 +29,14 @@ def sort_contour (frame, method = 'XA'):    # XA : sort X_axis Ascending , XD : 
 
     o_frame = frame.copy()
 
-    # Increase constrast
-    #alpha = 1.3         # constrast : 1.0 -> 3.0
-    #beta = 0            # brightness : 0 -> 100
-    #aj_img = cv2.convertScaleAbs(frame, alpha = alpha, beta = beta)
-
-    # reduce highlight 
-    hsvImg = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    hsvImg[...,2] = hsvImg[...,2]*0.8
-    hi_img = cv2.cvtColor(hsvImg,cv2.COLOR_HSV2BGR)
-
-    gray_img = cv2.cvtColor(hi_img, cv2.COLOR_BGR2GRAY)
+    frame = preprocess_image(frame)
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-    thresh_img = ~cv2.threshold(blur_img, 130, 255, cv2.THRESH_BINARY)[1]
-    
-    #kernel = np.ones((5,5),np.uint8)
-    #erode_img = cv2.erode(thresh_img, kernel, 1)
+
+    if dark_mode :
+        thresh_img = cv2.threshold(blur_img, 130, 255, cv2.THRESH_BINARY)[1]
+    else : 
+        thresh_img = ~cv2.threshold(blur_img, 130, 255, cv2.THRESH_BINARY)[1]
 
     cnts = cv2.findContours(thresh_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
@@ -65,7 +58,7 @@ def sort_contour (frame, method = 'XA'):    # XA : sort X_axis Ascending , XD : 
                 if (M["m00"] != 0) : 
                     cX = int(M["m10"] / M["m00"])
                     cY = int(M["m01"] / M["m00"])
-                    # draw the countour number on the image
+                    #draw the countour number on the image
                     cv2.putText(o_frame, "#{}".format(i + 1), (cX - 10, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
                 i = i + 1
     return o_frame
